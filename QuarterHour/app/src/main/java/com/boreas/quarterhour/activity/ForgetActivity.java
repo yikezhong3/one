2006b053410df3435c2dd4cb4c2b6c19076af4e5
zhouxua <32317191+zhouxua@users.noreply.github.com>
2018-01-24 09:37:55 +0800

    hh

--- QuarterHour/app/src/main/java/com/boreas/quarterhour/activity/ForgetActivity.java
+++ QuarterHour/app/src/main/java/com/boreas/quarterhour/activity/ForgetActivity.java
@@ -2,6 +2,7 @@ package com.boreas.quarterhour.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.boreas.quarterhour.R;
 
@@ -11,5 +12,6 @@ public class ForgetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget);
+        Toast.makeText(this,"",Toast.LENGTH_SHORT).show();
     }
 }
