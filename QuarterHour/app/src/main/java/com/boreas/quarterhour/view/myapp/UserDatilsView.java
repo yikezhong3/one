0e0ecf8edf3f5bd577eaf7f7b1bccb366f5a1513
345084128 <hai14>
2018-01-30 20:35:03 +0800

    1/30.2

---
+++ QuarterHour/app/src/main/java/com/boreas/quarterhour/view/myapp/UserDatilsView.java
@@ -0,0 +1,11 @@
+package com.boreas.quarterhour.view.myapp;
+
+import com.boreas.quarterhour.model.UserDetailBean;
+
+/**
+ * Created by 123 on 2018/1/30.
+ */
+
+public interface UserDatilsView {
+    public void onSuccess(UserDetailBean bean);
+}
