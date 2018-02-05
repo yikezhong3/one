b451ea96f737648c1db7636a8eec3026c817f934
wuzeqiang2017 <2320948671@qq.com>
2018-02-05 10:11:31 +0800

    整体修改添加图片头像

--- QuarterHour/app/src/main/java/com/boreas/quarterhour/view/adapter/MyRecyclerAdapter3.java
+++ QuarterHour/app/src/main/java/com/boreas/quarterhour/view/adapter/MyRecyclerAdapter3.java
@@ -11,11 +11,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.boreas.quarterhour.R;
-import com.boreas.quarterhour.model.AttentionBean;
 import com.boreas.quarterhour.model.SelectBean;
 import com.bumptech.glide.Glide;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by dream on 2018/2/3.
@@ -45,7 +45,20 @@ public class MyRecyclerAdapter3 extends RecyclerView.Adapter<MyRecyclerAdapter3.
 
         holder.tv1.setText( mDatas.get(position).getUsername());
         holder.tv2.setText( mDatas.get(position).getCreatetime());
-        Glide.with(mContext).load(mDatas.get(position).getIcon()).placeholder(R.mipmap.ic_launcher).into(holder.img);
+
+        String[] Usicon = {
+                "http://p1.qq181.com/cms/120503/2012050322011423334.jpg",
+                "http://p1.so.qhimgs1.com/t014803de8c7e2a2b7d.jpg",
+                "http://www.16sucai.com/uploadfile/2013/0630/20130630095526852.jpg",
+                "http://v1.qzone.cc/avatar/201406/12/14/00/539941ff03ebf693.png%21200x200.jpg",
+                "http://www.qqw21.com/article/UploadPic/2012-10/2012102592515140.jpg",
+                "http://pic.ik123.com/uploads/allimg/131021/16-131021141R6.jpg",
+                "http://img2.touxiang.cn/file/20170106/c8426f3a7e8e38e0d5869042acae0283.jpg",
+        };
+        Random ran = new Random();
+        int i = ran.nextInt(Usicon.length);
+        String userIcon = Usicon[i];
+        Glide.with(mContext).load(userIcon).into(holder.img);
         holder.btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,7 +71,7 @@ public class MyRecyclerAdapter3 extends RecyclerView.Adapter<MyRecyclerAdapter3.
     @Override
     public MyRecyclerAdapter3.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View view = inflater.inflate(R.layout. item_select,parent, false);
+        View view = inflater.inflate(R.layout.item_select,parent, false);
         MyRecyclerAdapter3.MyViewHolder holder= new MyRecyclerAdapter3.MyViewHolder(view);
         return holder;
     }
